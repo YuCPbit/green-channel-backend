001b8f4daa19d6617bbe3445fd4f774da3b795ef
Luliiiii526 <kangyutong@stu.scu.edu.cn>
2026-07-23 10:30:35 +0800

    Salary修改完成

--- workstudy-service/src/main/java/edu/greenchannel/workstudy/service/WorkStudySalaryService.java
+++ workstudy-service/src/main/java/edu/greenchannel/workstudy/service/WorkStudySalaryService.java
@@ -2,14 +2,28 @@ package edu.greenchannel.workstudy.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import edu.greenchannel.workstudy.entity.WorkStudySalary;
+import java.math.BigDecimal;
 import java.time.YearMonth;
 
 public interface WorkStudySalaryService extends IService<WorkStudySalary> {
 
     /**
      * 核算指定年月的薪酬
-     * @param yearMonth 年月（如 2026-07）
-     * @return 生成的薪酬记录数
      */
     int calculateMonthlySalary(YearMonth yearMonth);
+
+    /**
+     * 部门确认薪酬
+     */
+    void confirmByDepartment(Long salaryId, Long deptUserId, BigDecimal confirmedAmount);
+
+    /**
+     * 资助中心审批薪酬
+     */
+    void approveBySchool(Long salaryId, Long schoolUserId, BigDecimal finalAmount);
+
+    /**
+     * 标记薪酬已发放
+     */
+    void markAsPaid(Long salaryId, Long operatorId);
 }
