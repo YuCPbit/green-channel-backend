01ede9ae5c910ac9ddceb6244bf4c57788c7013c
YuCPbit <YuCPbit@users.noreply.github.com>
2026-07-24 11:43:13 +0800

    fix(INT-T01): keep Boot 4 MyBatis autoconfiguration

--- workstudy-service/src/main/java/edu/greenchannel/workstudy/config/MybatisPlusConfig.java
+++ workstudy-service/src/main/java/edu/greenchannel/workstudy/config/MybatisPlusConfig.java
@@ -3,14 +3,8 @@ package edu.greenchannel.workstudy.config;
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
-import com.baomidou.mybatisplus.extension.spring.MybatisSqlSessionFactoryBean;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.mybatis.spring.SqlSessionTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.sql.DataSource;
 
 /**
  * MyBatis-Plus 配置类
@@ -18,9 +12,6 @@ import javax.sql.DataSource;
  */
 @Configuration
 public class MybatisPlusConfig {
-
-    @Autowired
-    private DataSource dataSource;
 
     /**
      * 添加 MyBatis-Plus 拦截器
@@ -42,18 +33,5 @@ public class MybatisPlusConfig {
         interceptor.addInnerInterceptor(paginationInterceptor);
 
         return interceptor;
-    }
-
-    @Bean
-    public SqlSessionFactory sqlSessionFactory(MybatisPlusInterceptor mybatisPlusInterceptor) throws Exception {
-        MybatisSqlSessionFactoryBean factoryBean = new MybatisSqlSessionFactoryBean();
-        factoryBean.setDataSource(dataSource);
-        factoryBean.setPlugins(mybatisPlusInterceptor);
-        return factoryBean.getObject();
-    }
-
-    @Bean
-    public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
-        return new SqlSessionTemplate(sqlSessionFactory);
     }
 }
